9b18e444e2f4b9198870bc86c8ade2a7ca2f1b57
Lucas <lucashdk2@gmail.com>
2026-01-07 13:44:10 -0300

    chore: bump CLI version to 1.2.1

--- src/commands/config.py
+++ src/commands/config.py
@@ -13,7 +13,7 @@ import yaml
 class CorplangConfig:
     """Configuration with cli-level"""
 
-    VERSION = "1.2.0"
+    VERSION = "1.2.1"
     CACHE_DIR = ".corplang-cache"
     MANIFEST_FILE = "manifest.json"
     CONFIG_FILE = "language_config.yaml"
